b388a3d5227fe357b395c00286d857dfaaaa9357
Ubuntu <ubuntu@ip-172-31-6-180.ap-northeast-1.compute.internal>
2021-04-07 21:55:11 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  #root to: 'tasks#index'
+  
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 
   resources :tasks
 end
