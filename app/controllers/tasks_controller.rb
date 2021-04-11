049cb397dd9b507f40a124560b4dcf38327ecd9b
Ubuntu <ubuntu@ip-172-31-6-180.ap-northeast-1.compute.internal>
2021-04-11 03:54:32 +0000

    yuya 9

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def index
-  @tasks = Task.all.page(params[:page])
+   if logged_in?
+    @task = current_user.tasks.build  # form_with 用
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])  end
   end
 
   def show
